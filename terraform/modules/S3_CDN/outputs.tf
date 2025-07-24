ff208771edf11822958fbd7f1cb1cf7dfa69ddf0
Hadi Golkarieh <hadigol@gmail.com>
2025-07-24 10:47:55 -0700

    fix

--- terraform/modules/S3_CDN/outputs.tf
+++ terraform/modules/S3_CDN/outputs.tf
@@ -2,3 +2,7 @@ output "cdn_url"{
     description = "endpoint"
     value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output cdn_id{
+    value = aws_cloudfront_distribution.s3_distribution.id
+}
--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -1,3 +1,11 @@
-output "endpot"{
+output "endpoint"{
     value = module.s3_cdn.cdn_url
+}
+
+output "bucket_name"{
+    value="${var.env}-${var.bucket_name}"
+}
+
+output "cloudfront_distribution_id"{
+    value = module.s3_cdn.cdn_id
 }
